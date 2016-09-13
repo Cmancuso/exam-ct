f4d6440f7530b0b44e03b4908593ee831210a7ed
Cmancuso <Christopher.a.mancuso@gmail.com>
2016-09-13 19:03:07 -0400

    modularized a bit, made the display its own method

--- index.php
+++ index.php
@@ -17,48 +17,73 @@
 
  		// method declaration
     	public function animate($speed,$init) {
-    		echo "<div style ='font:11px/21px Courier,tahoma,sans-serif;'>";	
-        	$str_array = str_split($init);												//convert the string into an array
-        	for ($i=0; $i<ceil((count($str_array)/($speed))); $i++){					//iterate until all particles will be off screen
-        		$next_array = str_split(str_repeat(".",count($str_array)));				//next_array defaults to all empty particles, and will be filled in later
-        		$display_string = implode(" ", $str_array);								//convert array back to a string - sub all characters for X's
+    		$return_array = Array();
+    		//convert the string into an array
+        	$str_array = str_split($init);	
+        	//iterate until all particles will be off screen
+        	for ($i=0; $i<(count($str_array)/($speed)); $i++){
+        		//next_array defaults to all empty particles, and will be filled in later				
+        		$next_array = str_split(str_repeat(".",count($str_array)));				
+        		//convert array back to a string - sub all characters for X's
+        		$display_string = implode(" ", $str_array);	
         		$display_string = str_replace("LR", "X", $display_string);
         		$display_string = str_replace("L", "X", $display_string);
         		$display_string = str_replace("R", "X", $display_string);
-        		
-        		echo $display_string;
-	        	echo "<br />";
-
-        		for($j=0; $j<count($str_array); $j ++){									//iterate through each array element
-	        		if($str_array[$j] == 'L' && ($j-$speed>=0)){						//check for left and check if next element is in array scope
-	        			if($next_array[$j-$speed] == 'R'){								//check for overlapping particles
+        		//push to our final array
+        		array_push($return_array,$display_string);
+        		//exit condition
+        		if( implode(" ",$next_array) == $display_string){
+        			return $return_array;
+        		}
+        		//iterate through each array element
+        		for($j=0; $j<count($str_array); $j ++){
+        			//check for left and check if next element is in array scope
+	        		if($str_array[$j] == 'L' && ($j-$speed>=0)){
+	        			//check for overlapping particles						
+	        			if($next_array[$j-$speed] == 'R'){								
 	        				$next_array[$j - $speed] ='LR';
 	        			}
-	        			else{															//otherwise reassign 'L' to next array
+	        			//otherwise reassign 'L' to next array
+	        			else{															
 	        				$next_array[$j - $speed] = 'L';
 	        			}
 	        		}
-	        		elseif($str_array[$j] == 'R' && (($j+$speed)<count($str_array))){	//check for right and if next element is in scope
-	        			if($next_array[$j+$speed] == 'L'){								//check for overlap
+	        		//check for right and if next element is in scope
+	        		elseif($str_array[$j] == 'R' && (($j+$speed)<count($str_array))){	
+	        			//check for overlap
+	        			if($next_array[$j+$speed] == 'L'){								
 	        				$next_array[$j +$speed] ='LR';
 	        			}
-	        			else{															//assign 'R' to next array
+	        			//assign 'R' to next array
+	        			else{															
 	        				$next_array[$j + $speed] = 'R';
 	        			}
 	        			
 	        		}
-	        		elseif($str_array[$j] == 'LR'){										//if overlap particle, move in both directions
+	        		//if overlap particle, move in both directions
+	        		elseif($str_array[$j] == 'LR'){										
 	        			$next_array[$j + $speed] = 'R';
 	        			$next_array[$j - $speed] = 'L';
 	        		}
 	        	}
-	        	$str_array = $next_array;												//set the str(display) array = to the generated array
+	        	//set the str(display) array = to the generated array
+	        	$str_array = $next_array;												
         	}
-        	echo "</div>";
+  
+    	}
+    	/*******SIMPLE DISPLAY FUNCTION***********/
+    	function display($array){
+    		echo "<div style ='font:11px/21px Courier,tahoma,sans-serif;'>";
+    		for($i=0;$i<sizeof($array); $i++){
+    			echo $array[$i];
+    			echo "<br />";
+    		}
+	        echo "</div>";   
     	}
  	}
  		$instance = new Animation;
- 		$instance->animate(2, "LRLR.LRLR");
+ 		$return_array = $instance->animate(2, "L.R...");
+ 		$instance->display($return_array);
 	?> 
  </body>
 </html>
